58bc0286712cebbc23749b8b1d5fb120e5a44477
iarmolla <ivan.armolla@gmail.com>
2023-02-28 08:26:19 -0300

    add protected route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Chat from './components/Chat';
 import Login from './components/Login'
 import Register from './components/Register';
-
+import ProtectedRoute from './components/ProtectedRoute'
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/chat' element={<ProtectedRoute>
+            <Chat />
+          </ProtectedRoute>}>
+          </Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/chat" element={<Chat />}></Route>
-          <Route path="*" element={<Login/>}></Route>
+          <Route path="*" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
