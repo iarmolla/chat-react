b5174572a7d0a82ff121d33c3c4e69a4222f496f
iarmolla <ivan.armolla@gmail.com>
2023-02-28 05:34:18 -0300

    add formik

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,13 +3,14 @@ import { Link, useNavigate } from 'react-router-dom'
 import '../styles/login.css'
 import { singIn } from '../actions/auth'
 import { useDispatch } from 'react-redux'
+import { Formik } from 'formik'
 
 function Login() {
     const dispatch = useDispatch()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const navigate = useNavigate()
-  
+
     return (
         <div className='h-screen w-screen flex items-center justify-center'>
             <section className="min-h-screen flex items-stretch text-white">
@@ -39,45 +40,79 @@ function Login() {
                         <h1 className="my-6 text-4xl">
                             Sign in
                         </h1>
-                        <form action="POST" onSubmit={(e) => {
-                            e.preventDefault()
-                            const user = {
-                                email: email,
-                                password: password
-                            }
-                            dispatch(singIn(user, navigate))
-                        }} className=" w-full px-4 lg:px-0 mx-auto">
-                            <div className="pb-2 pt-4">
-                                <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" placeholder="Email" className="block w-full p-4 text-lg rounded-sm bg-black" />
-                            </div>
-                            <div className="pb-2 pt-4">
-                                <input onChange={(e) => setPassword(e.target.value)} className="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password" id="password" placeholder="Password" />
-                            </div>
-                            <div className="text-left text-gray-400 hover:underline hover:text-gray-100">
-                                <Link to="/register">Register?</Link>
-                            </div>
-                            <div>
-                                <button type="submit" class="group relative flex w-full mt-3 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                        <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                    Login
-                                </button>
-                            </div>
-                            <div className="p-4 text-center right-0 left-0 flex justify-center space-x-4 mt-16 lg:hidden ">
-                                <a href="#">
-                                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></svg>
-                                </a>
-                                <a href="#">
-                                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" /></svg>
-                                </a>
-                                <a href="#">
-                                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" /></svg>
-                                </a>
-                            </div>
-                        </form>
+                        <Formik
+                            initialValues={{
+                                email: '',
+                                password: '',
+                            }}
+                            validate={(values) => {
+                                let errors = {}
+                                if (!values.email) {
+                                    errors.email = "* Missing field*"
+                                } else if (
+                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                ) {
+                                    errors.email = "* Invalid email*"
+                                }
+
+                                if (!values.password) {
+                                    errors.password = "* Missing field*"
+                                }
+
+                                return errors
+                            }}
+                            onSubmit={async (values) => {
+                                dispatch(singIn(values, navigate))
+                            }}
+                        >
+                            {({ handleSubmit,
+                                errors,
+                                values,
+                                touched,
+                                handleChange,
+                                handleBlur
+                            }) => (
+                                <form action="POST" onSubmit={handleSubmit} className=" w-full px-4 lg:px-0 mx-auto">
+                                    <div className="pb-2 pt-4">
+                                        <input onChange={handleChange} value={values.email} onBlur={handleBlur} type="email" name="email" id="email" placeholder="Email" className="block w-full p-4 text-lg rounded-sm bg-black" />
+                                    </div>
+                                    <div className='w-full text-left'>
+                                        <label className={`${touched.email && errors.email ? 'error-text text-left  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.email && errors.email}</label>
+                                    </div>
+                                    <div className="pb-2 pt-4">
+                                        <input onChange={handleChange} value={values.password} onBlur={handleBlur} className="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password" id="password" placeholder="Password" />
+                                    </div>
+                                    <div className='w-full text-left'>
+                                        <label className={`${touched.password && errors.password ? 'error-text text-left  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.password && errors.password}</label>
+
+                                    </div>
+                                    <div className="text-left mt-3 text-gray-400 hover:underline hover:text-gray-100">
+                                        <Link to="/register">Register?</Link>
+                                    </div>
+                                    <div>
+                                        <button type="submit" class="group relative flex w-full mt-3 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                                <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                                                </svg>
+                                            </span>
+                                            Login
+                                        </button>
+                                    </div>
+                                    <div className="p-4 text-center right-0 left-0 flex justify-center space-x-4 mt-16 lg:hidden ">
+                                        <a href="#">
+                                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></svg>
+                                        </a>
+                                        <a href="#">
+                                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" /></svg>
+                                        </a>
+                                        <a href="#">
+                                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" /></svg>
+                                        </a>
+                                    </div>
+                                </form>
+                            )}
+                        </Formik>
                     </div>
                 </div>
             </section>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { createUser } from '../actions/users'
+import { Formik } from 'formik'
 
 function Register() {
     const [email, setEmail] = useState()
@@ -17,50 +18,78 @@ function Register() {
                     <div>
                         <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-white">Register your account</h2>
                     </div>
-                    <form class="mt-8 space-y-6" method='POST' onSubmit={(e) => {
-                        e.preventDefault()
-                        const user = {
-                            username: username,
-                            email: email,
-                            password: password
-                        }
-                        dispatch(createUser(user, navigate))
-                    }}>
-                        <div class="-space-y-px rounded-md shadow-sm">
-                            <div className="pb-2 pt-4">
-                                <input onChange={(e) => {
-                                    setEmail(e.target.value)
-                                }} type="email" name="email" id="email" placeholder="Email" className="text-white block w-full p-4 text-lg rounded-sm bg-black" />
-                            </div>
-                            <div className="pb-2 pt-4">
-                                <input onChange={(e) => {
-                                    setUserName(e.target.value)
-                                }} type="username" name="username" id="username" placeholder="Username" className="text-white block w-full p-4 text-lg rounded-sm bg-black" />
-                            </div>
-                            <div className="pb-2 pt-4">
-                                <input onChange={(e) => {
-                                    setPassword(e.target.value)
-                                }} className="block w-full p-4 text-lg rounded-sm text-white bg-black" type="password" name="password" id="password" placeholder="Password" />
-                            </div>
-                        </div>
+                    <Formik
+                        initialValues={{
+                            username: '',
+                            email: '',
+                            password: '',
+                        }}
+                        validate={(values) => {
+                            let errors = {}
+                            const passwordIsValid = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+                            if (!values.email) {
+                                errors.email = "* Missing field*"
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                            ) {
+                                errors.email = "* Invalid email*"
+                            }
+                            if (!values.username) {
+                                errors.username = "* Missing field*"
+                            }
+                            if (!values.password) {
+                                errors.password = "* Missing field*"
+                            }
+                            else if (!passwordIsValid.test(values.password)) {
+                                errors.password = "* Minimum eight characters, at least one letter and one number *"
 
-                        <div class="flex items-center justify-between">
-                            <div class="text-sm">
-                                <Link to="/login" class="font-medium text-indigo-600 hover:text-indigo-500">Do you have an account?</Link>
-                            </div>
-                        </div>
-
-                        <div>
-                            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                                    </svg>
-                                </span>
-                                Register
-                            </button>
-                        </div>
-                    </form>
+                            }
+                            return errors
+                        }}
+                        onSubmit={async (values) => {
+                            dispatch(createUser(values, navigate))
+                        }}
+                    >
+                        {({ handleSubmit,
+                            errors,
+                            values,
+                            touched,
+                            handleChange,
+                            handleBlur
+                        }) => (
+                            <form class="mt-8 space-y-6" method='POST' onSubmit={handleSubmit}>
+                                <div class="-space-y-px rounded-md shadow-sm">
+                                    <div className="pb-2 pt-4">
+                                        <input onChange={handleChange} value={values.email} onBlur={handleBlur} type="email" name="email" id="email" placeholder="Email" className="text-white block w-full p-4 text-lg rounded-sm bg-black" />
+                                    </div>
+                                    <label className={`${touched.email && errors.email ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.email && errors.email}</label>
+                                    <div className="pb-2 pt-4">
+                                        <input onChange={handleChange} value={values.username} onBlur={handleBlur} type="username" name="username" id="username" placeholder="Username" className="text-white block w-full p-4 text-lg rounded-sm bg-black" />
+                                    </div>
+                                    <label className={`${touched.username && errors.username ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.username && errors.username}</label>
+                                    <div className="pb-2 pt-4">
+                                        <input onChange={handleChange} value={values.password} onBlur={handleBlur} className="block w-full p-4 text-lg rounded-sm text-white bg-black" type="password" name="password" id="password" placeholder="Password" />
+                                    </div>
+                                    <label className={`${touched.password && errors.password ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.password && errors.password}</label>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <div class="text-sm">
+                                        <Link to="/login" class="font-medium text-indigo-600 hover:text-indigo-500">Do you have an account?</Link>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                        Register
+                                    </button>
+                                </div>
+                            </form>
+                        )}
+                    </Formik>
                 </div>
             </div>
 
