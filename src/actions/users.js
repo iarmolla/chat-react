b6895e49dd1b56aa7a665951f758711f71a606ed
iarmolla <ivan.armolla@gmail.com>
2023-02-28 08:26:57 -0300

    removing create user

--- src/actions/users.js
+++ src/actions/users.js
@@ -47,29 +47,3 @@ export function getUserFailed(error) {
     
 }
 
-export function createUserError(error) {
-    return {
-        type: 'CREATE_USER_ERROR',
-        error
-    }
-}
-
-export function createUser(user, navigate) {
-    return async (dispatch) => {
-        try {
-            const response = await axios.post('http://localhost:3030/signup', user)
-            dispatch(createUserSuccess(response.data))
-            navigate('/chat')
-
-        } catch (error) {
-            dispatch(createUserError(error.message))
-        }
-    }
-}
-
-export function createUserSuccess(message) {
-    return {
-        type: 'CREATE_USER_SUCCESS',
-        message
-    }
-}
