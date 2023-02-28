94e2588625051d8933611258b76422816807c362
iarmolla <ivan.armolla@gmail.com>
2023-02-28 08:29:59 -0300

    add to store auth & createUser

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,13 @@ import { combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import usersReducer from '../reducers/usersReducer'
 import authReducer from '../reducers/authReducer'
-
+import createUserReducer from '../reducers/createUserReducer'
 import { configureStore } from '@reduxjs/toolkit'
 
 const reducer = combineReducers({
     users: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    createUser: createUserReducer
 })
 
 export const store = configureStore(
