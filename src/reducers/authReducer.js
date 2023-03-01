d5b9b868e150f410cdd59084445ca509e5e4cb7c
iarmolla <ivan.armolla@gmail.com>
2023-03-01 07:42:30 -0300

    fix state

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,11 +3,9 @@ const initialState = []
 function auth(state = initialState, action) {
     switch (action.type) {
         case 'LOGIN_USER_SUCCESS':
-            const { token } = action.message
+            const { token, auth } = action.message
             window.localStorage.setItem('token', token)
-            return {
-                ...action.message
-            }
+            return auth
         case 'LOGIN_USER_ERROR':
             let { message } = action.message
             message = 'Username or password is not correct'
--- src/reducers/createUserReducer.js
+++ src/reducers/createUserReducer.js
@@ -3,7 +3,8 @@ const initialState = []
 function createUserReducer(state = initialState, action) {
     switch (action.type) {
         case 'CREATE_USER_SUCCESS':
-            return action.message 
+            const { auth } = action.message
+            return auth
         case 'CREATE_USER_FAILED':
             return action.error.message 
         default: {
