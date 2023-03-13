13f344e13a0759fbb3f2f5bbcc083c47f94ac0da
iarmolla <ivan.armolla@gmail.com>
2023-03-13 20:47:03 -0300

    add messages

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import usersReducer from '../reducers/usersReducer'
 import authReducer from '../reducers/authReducer'
 import createUserReducer from '../reducers/createUserReducer'
 import { configureStore } from '@reduxjs/toolkit'
+import messagesReducer from '../reducers/messagesReducer'
 
 const reducer = combineReducers({
     users: usersReducer,
     auth: authReducer,
-    createUser: createUserReducer
+    createUser: createUserReducer,
+    messages: messagesReducer
 })
 
 export const store = configureStore(
