0b9264d4076f68aa90e0d334ee59849824618657
iarmolla <ivan.armolla@gmail.com>
2023-03-13 20:46:39 -0300

    add rooms

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,58 +1,65 @@
 import React, { useMemo } from 'react'
 import io from 'socket.io-client'
+import save from '../actions/messages'
 import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { useDispatch } from 'react-redux'
 import { getUser, getUsers } from '../actions/users'
+import { getMessagesFetch, saveMessages } from '../actions/messages'
+
 import { connect } from 'react-redux'
 import usersSelector from '../selectors/usersSelector'
+import messages from '../selectors/messages'
 
-const socket = io('http://localhost:3030')
 const mapStateToProps = state => {
     return {
-        getUsers: usersSelector(state)
+        getUsers: usersSelector(state),
+        getMessages: messages(state)
     }
 }
+
 function Chat(...props) {
+    const [room, setRoom] = useState('general')
+    const socket = io('http://localhost:3030', {
+        query: {
+            room: room
+        }
+    })
     const dispatch = useDispatch()
     const [query, setQuery] = useState('')
     const [message, setMessage] = useState('')
-    const [messages, setMessages] = useState([])
     useEffect(() => {
         if (!props[0].getUsers.length) {
             dispatch(getUsers())
         }
     }, [dispatch])
+  
     useEffect(() => {
-        socket.on('received', (newMessage) => {
-            const aux = [{ ...newMessage }]
-            setMessages([...messages, ...aux])
+        socket.on('received', () => {
+            dispatch(getMessagesFetch(room))
         })
-        let index = messages.length - 1 .toString()
+        let index = props[0].getMessages.length - 1 .toString()
         index = `${index} message`
         const element = document.getElementById(index)
         element?.scrollIntoView()
-        
-    }, [messages])
+    }, [socket])
+    useEffect(() => {
+        socket.emit('join', room)
+        dispatch(getMessagesFetch(room))
+    }, [room])
     const handleSubmit = (e) => {
         e.preventDefault()
         if (message !== '') {
-            socket.emit('message', message)
-            const newMessage = {
-                body: message,
-                from: 'me'
-            }
-            const aux = [{ ...newMessage }]
-            setMessages([...messages, ...aux])
+            socket.emit('message', message, room, window.localStorage.getItem('email'))
             setMessage('')
         }
     }
-    const navbar = useMemo(()=> {
-        return <Navbar/>
-    },[])
+    const navbar = useMemo(() => {
+        return <Navbar />
+    }, [])
     return (
         <>
-        {navbar}
+            {navbar}
             <div className="container-chat mt-24">
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <section className="container-section">
@@ -70,6 +77,23 @@ function Chat(...props) {
                                 }}
                             />
                             <div className="h-full chat overflow-y-scroll">
+                                <div className='text-white flex flex-col gap-3 mb-5'>
+                                    <h6 className="container-item-- overflow-ellipsis">Rooms</h6>
+                                    <h5 className='contact-item' onClick={async (e) => {
+                                        setRoom('general')
+
+
+                                    }}>General</h5>
+                                    <h5 className='contact-item' onClick={async (e) => {
+                                        setRoom('games')
+
+                                    }}>Games</h5>
+                                    <h5 className='contact-item' onClick={async (e) => {
+                                        setRoom('music')
+
+
+                                    }}>Music</h5>
+                                </div>
                                 <div>
                                     <h6 className="container-item-- overflow-ellipsis">Online</h6>
                                     <div className="contact h-9">
@@ -85,21 +109,21 @@ function Chat(...props) {
                             </div>
                         </div>
                         <div className="w-100">
-                            <p className="chat-title-user">GROUP</p>
+                            <p className="chat-title-user">{room}</p>
                             <div className="chat-user-text">
                                 <div className="section" id='message'>
                                     {
-                                        messages.map((item, index) => {
-                                            if (item.from != 'me') {
+                                        props[0].getMessages?.map((item, index) => {
+                                            if (item.email != window.localStorage.getItem('email')) {
                                                 return (
                                                     <div key={index} className='section-user-text'>
-                                                        <button id={`${index} message`} className='text-button-user'>{item?.body}</button>
+                                                        <button id={`${index} message`} className='text-button-user'>{item?.message}</button>
                                                     </div>
                                                 )
                                             } else {
                                                 return (
                                                     <div key={index} className='user-text '>
-                                                        <p id={`${index} message`} className='text-button-user text-button-user--'>{item?.body}</p>
+                                                        <p id={`${index} message`} className='text-button-user text-button-user--'>{item?.message}</p>
                                                     </div>
                                                 )
                                             }
@@ -113,6 +137,7 @@ function Chat(...props) {
                                     value={`${message != '' ? message : ''}`}
                                     className="chat-footer w-32 sm:w-full"
                                     type="text"
+                                    maxLength={23}
                                     placeholder="send"
                                 />
                                 <button onClick={(e) => handleSubmit(e)}>Send</button>
